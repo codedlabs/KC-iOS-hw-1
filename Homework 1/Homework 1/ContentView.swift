0bad4c4a24b8222168e14e2c7df1c6e6a8c50e03
Adnan-Alfadhli <adnan2000611@gmail.com>
2022-03-08 20:16:00 +0300

    .

---
+++ Homework 1/Homework 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework 1
+//
+//  Created by Adnan Alfadhli on 08/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
