b0d4ec54d4d8278a20c89a6d5fb63524577f6ea5
Ahmed Ikrami <99684336+b22-dev@users.noreply.github.com>
2022-08-01 14:34:25 +0200

    favMovies

---
+++ FavMovies/FavMovies/FavMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavMoviesApp.swift
+//  FavMovies
+//
+//  Created by HEBA AHMED on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FavMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
